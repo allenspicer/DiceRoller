feacb924340eeb041da9e5fde481d90af2063c21
Allen Spicer <allenspicer@gmail.com>
2016-05-24 11:58:38 -0400

    Update

--- DiceRoller/DCVC+datasource.swift
+++ DiceRoller/DCVC+datasource.swift
@@ -19,7 +19,19 @@ extension DiceCollectionViewController{
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("dieCell", forIndexPath: indexPath)
         
+        let keyArray = Array(self.diceHolder.keys)
+        let selectedDie = self.diceHolder[keyArray[indexPath.row]]
+        
+        let label =  UILabel(frame: cell.bounds)
+        label.text = String(selectedDie!.currentValue)
+        label.textAlignment = .Center
+        for subView in cell.contentView.subviews {
+            subView.removeFromSuperview()
+        }
+        
+        cell.contentView.addSubview(label)
         cell.contentView.backgroundColor = .blueColor()
+
         
         return cell
     }
